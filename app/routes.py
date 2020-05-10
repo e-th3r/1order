3d5c3c997918f050952c184c9b197d89da6dae02
MaximKa420 <maxim.perchikov@gmail.com>
2020-05-10 14:50:17 +0300

    New files architecture, testing authorization

---
+++ app/routes.py
@@ -0,0 +1,54 @@
+from flask import render_template
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from flask_login import login_user, logout_user, current_user, login_required
+@app.route("/") 
+@app.route("/index.html")
+def index():
+	return render_template("index.html")
+@app.route("/whatis.html")
+def whatis():
+	return render_template("whatis.html")
+@app.route("/aboutproject.html")
+def aboutproject():
+	return render_template("aboutproject.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+if __name__ == '__name__':
+    app.run()
