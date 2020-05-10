a5377c921587329d0d435aeaf21e446cc97cd351
MaximKa420 <maxim.perchikov@gmail.com>
2020-05-10 17:06:48 +0300

    Authorization test2

--- app/models.py
+++ app/models.py
@@ -24,13 +24,3 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
