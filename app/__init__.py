f47160a67360a3e41ed63db7089146c8ed55209d
MaximKa420 <maxim.perchikov@gmail.com>
2020-06-04 19:39:07 +0300

    Deploying to Heroku#3

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 
-app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- order.py
+++ order.py
@@ -1,6 +1,7 @@
-from app import app, db
+from app import db
 from app.models import User
 
+app = Flask(__name__)
 
 @app.shell_context_processor
 def make_shell_context():
