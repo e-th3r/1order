1c8e7b8b59d1f6884da3aa30c288ee77d3900acd
MaximKa420 <maxim.perchikov@gmail.com>
2020-07-26 16:05:20 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, redirect, url_for, request
-from app import app, db
-from app.forms import LoginForm, RegistrationForm, PostForm
-from app.models import User, Post
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 @app.route("/", methods=['GET', 'POST']) 
 @app.route("/index.html", methods=['GET', 'POST'])
 def index():
