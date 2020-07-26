39e972c5ad6f2abffe3588211e2e2b5db83272a8
MaximKa420 <maxim.perchikov@gmail.com>
2020-07-26 16:15:16 +0300

    \\||

--- app/routes.py
+++ app/routes.py
@@ -14,16 +14,16 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-        posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-        ]
+    posts = [
+    {
+        'author': {'username': 'John'},
+        'body': 'Beautiful day in Portland!'
+    },
+    {
+        'author': {'username': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }
+    ]
     return render_template("index.html", title='Home Page', form=form, posts=posts)
 @app.route("/whatis.html")
 def whatis():
